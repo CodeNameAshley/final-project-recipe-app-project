4717867a9f4695b14fc457d0d31d584862c3e410
Khalid Nur <kingnur36@gmail.com>
2022-03-03 20:51:43 +0000

    added the searchResults component with proptypes and dummy data

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,10 +1,23 @@
 /* eslint-disable */
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function SearchResults() {
-  return (
-    <div>
-      <h1>Search Results Component</h1>
-    </div>
-  );
+export default function SearchResults({ data }) {
+  return data.map((recipe) => {
+    return (
+      <>
+        <div>{recipe.title}</div>
+        <div>{recipe.image}</div>
+      </>
+    );
+  });
 }
+
+SearchResults.propTypes = {
+  data: PropTypes.array({
+    recipe: PropTypes.object({
+      title: PropTypes.string,
+      image: PropTypes.string,
+    }),
+  }),
+};
