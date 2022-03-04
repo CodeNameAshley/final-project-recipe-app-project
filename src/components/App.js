4f6a533f99e6766c3244c299d2e053cf4eee7160
ashleyn <ashleynavo@googlemail.com>
2022-03-04 17:08:52 +0000

    RecipeInfo updated with props, prop data, and proptypes

--- src/components/App.js
+++ src/components/App.js
@@ -11,13 +11,6 @@ function App() {
       <h1>Recipe App Project</h1>
       <SearchBar />
       <SearchResults />
-      <RecipeInfo
-        title="Lasagna"
-        image="https://www.pexels.com/photo/lasagna-on-white-ceramic-plate-4079520/"
-        readyInMinutes={45}
-        servings={4}
-        original="lorem ipsum"
-      />
     </div>
   );
 }
