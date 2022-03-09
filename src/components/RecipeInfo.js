9e0fd5331f71a988e275e8c287d297ebe96d82cb
ashleyn <ashleynavo@googlemail.com>
2022-03-09 16:38:17 +0000

    Structuring recipe info as per design

--- src/components/RecipeInfo.js
+++ src/components/RecipeInfo.js
@@ -3,12 +3,16 @@ import PropTypes from "prop-types";
 
 export default function RecipeInfo({ result }) {
   const { image, title, instructions } = result;
+  console.log(instructions);
 
   return (
     <div className="recipe-info">
-      <div className="recipe-info__title">{title}</div>
-      <div className="recipe-info__image">{image}</div>
-      <div className="recipe-info__instructions">{instructions}</div>
+      <img className="recipe-info__image" src={image} alt={title} />
+      <div className="recipe-info__title">{title.toLowerCase()}</div>
+      <h1>follow these steps below</h1>
+      <div className="recipe-info__instructions">
+        <p>{instructions}</p>
+      </div>
     </div>
   );
 }
