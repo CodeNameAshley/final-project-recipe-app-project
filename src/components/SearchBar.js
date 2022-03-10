ea452dc6d437272c05351874fa34e095bbfe1b20
Khalid Nur <kingnur36@gmail.com>
2022-03-10 21:48:43 +0000

    adding loading spinner functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import * as ReactBootStrap from "react-bootstrap";
 import GetRecipeList from "../requests/GetRecipeList";
 import "../sassstyles/searchbar.scss";
 import background from "../images/banner_image.png";
 
 export default function SearchBar({ setSearchResults }) {
   const [value, setValue] = useState();
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setSearchResults(await GetRecipeList(value));
+    try {
+      setLoading(true);
+      const recipes = await GetRecipeList(value);
+      setSearchResults(recipes);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div className="searchBar">
+      {loading && <ReactBootStrap.Spinner animation="border" />}
       <img
         src={background}
         alt="food background"
@@ -22,11 +33,12 @@ export default function SearchBar({ setSearchResults }) {
       <form className="search-form" onSubmit={handleSubmit}>
         <input
           className="search-input"
+          disabled={loading}
           type="text"
           placeholder="Type your ingredients to find recipes"
           onChange={(e) => setValue(e.target.value)}
         />
-        <button className="submitButton" type="submit">
+        <button disabled={loading} className="submitButton" type="submit">
           Search
         </button>
       </form>
