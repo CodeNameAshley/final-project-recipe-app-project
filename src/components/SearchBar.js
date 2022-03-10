e9b4d74c950d6b824b51b24620636a67cd794d2d
eriq <e.abonie@gmail.com>
2022-03-10 19:39:27 +0000

    search items in array

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,11 +7,22 @@ import background from "../images/banner_image.png";
 export default function SearchBar({ setSearchResults }) {
   const [value, setValue] = useState();
 
+  const initialValue = [];
+  const [ingredients, setIngredients] = useState(initialValue);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     setSearchResults(await GetRecipeList(value));
   };
 
+  const handleAddItem = () => {
+    if (value) {
+      setIngredients([...ingredients, value]);
+      setValue("");
+    }
+  };
+
+  console.log(ingredients);
   return (
     <div className="searchBar">
       <img
@@ -19,13 +30,26 @@ export default function SearchBar({ setSearchResults }) {
         alt="food background"
         className="searchBar-background"
       />
+      <div>
+        <h1>chips</h1>
+        <div>
+          {
+          ingredients.map((ingredient) => <p>{ingredient}</p>)
+}
+
+        </div>
+      </div>
       <form className="search-form" onSubmit={handleSubmit}>
         <input
           className="search-input"
+          value={value}
           type="text"
           placeholder="Type your ingredients to find recipes"
           onChange={(e) => setValue(e.target.value)}
         />
+        <button className="submitButton" type="button" onClick={handleAddItem}>
+          Add
+        </button>
         <button className="submitButton" type="submit">
           Search
         </button>
